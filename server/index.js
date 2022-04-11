1b55826f64f5c0e71bb74c288561a32c3d01e693
= <arce.code@gmail.com>
2022-04-11 15:14:50 -0400

    Added mongoose connection to server

--- server/index.js
+++ server/index.js
@@ -10,3 +10,15 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+// This needs to be secured later 
+const CONNECTION_URL = 'mongodb+srv://javascript:javascript123@cluster0.smlqo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(()=>{console.log('Running on port 5000')})
+    .catch((error) => console.log(error.message));
+
+
+
+// https://www.mongodb.com/cloud/atlas
